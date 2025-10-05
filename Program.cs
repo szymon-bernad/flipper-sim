c2e2290b75ef0d26761061106fcac0df020b4c8c
Szymon Bernad <szymon.bernad@gmail.com>
2025-10-05 12:30:29 +0200

    using Radzen Blazor library

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 using FlipperSim;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddBlazorBootstrap();
 
+builder.Services.AddRadzenComponents();
 await builder.Build().RunAsync();
